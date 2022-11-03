344fa3c00c9f1ec7b4f3abbc502b609c9ef59e95
Ahmad <aalahmary0032@stu.kau.edu.sa>
2022-11-03 22:35:20 +0300

    init

---
+++ script1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to put PHP in HTML - Simple Example</title>
+</head>
+<body>
+    <p>
+        <?php echo "This message is from server side." ?>
+    </p>
+</body>
+</html>
